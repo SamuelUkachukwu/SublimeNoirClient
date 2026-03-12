20587560bce1a2104189d1d19f136576d585da8a
SamuelUkachukwu <shybold@yahoo.com>
2026-03-12 23:02:00 +0000

    Test run on axios fetch from home.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,51 @@
-import React from "react";
+// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import apiClient from "../api/axiosConfig";
+
+interface Product {
+  id: number;
+  name: string;
+  brand: string;
+  price: number;
+  sizeML: number;
+  quantity: number;
+}
 
 const Home = () => {
+  const [data, setData] = useState<Product[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await apiClient.get<Product[]>("/products");
+        setData(response.data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
-      <div>Home</div>
+      <section>
+        {data.map((product) => (
+          <div className="card" key={product.id}>
+            <h2>{product.name}</h2>
+            <p>
+              <strong>Brand:</strong> {product.brand}
+            </p>
+            <p>
+              <strong>Price:</strong> ${product.price}
+            </p>
+            <p>
+              <strong>Size:</strong> {product.sizeML} ML
+            </p>
+            <p>
+              <strong>Stock:</strong> {product.quantity}
+            </p>
+          </div>
+        ))}
+      </section>
     </>
   );
 };
