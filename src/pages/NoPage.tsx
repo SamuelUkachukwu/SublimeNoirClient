72ddf26ad9becc9351bd15ab002adb74392ba9c2
SamuelUkachukwu <shybold@yahoo.com>
2026-03-14 16:42:59 +0000

    update: 404 Error page.

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NoPage = () => {
   return (
-    <div>NoPage</div>
-  )
-}
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-black relative">
+        <div className="text-white text-center">
+          <h1 className="text-5xl mb-10 font-bold font-[kesiq]">SublimeNoir</h1>
 
-export default NoPage
+          <h1 className="text-3xl mb-10">404 PAGE NOT FOUND</h1>
+
+          <Link
+            to="/"
+            className="inline-block no-underline border border-white px-6 py-3 rounded hover:bg-white hover:text-black transition"
+          >
+            Head Back to Home Page
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NoPage;
